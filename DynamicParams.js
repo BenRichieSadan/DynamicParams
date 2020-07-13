a414eb0079aaa84cd1fab030eabb505de516aae4
Ben Richie Sadan <sbenrichie@gmail.com>
2020-07-13 16:33:26 -0700

    Revert "ver 1.1.0"
    This reverts commit 9b170bcbfd81246def469c84921abd172e55f529.

--- DynamicParams.js
+++ DynamicParams.js
@@ -1,77 +1,139 @@
 /**
  * Dynamic Params logical file
  * Developed By: Ben Richie Sadan @ Sisense
- * Version : 1.1.0
- * Last Modified Date : 13-July-2020
+ * Version : 1.0
+ * Last Modified Date : 22-June-2020
  */
 
-
 let widgetName = 'DynamicParams';
-let dynamicParamsData;
 
-function initializeDynamicParams(widget, element){
-  dynamicParamsData = {
-    fieldsForDynamicParams:[],
-    dynamicParamsInputs: [],
-    numOfValues: 0
-  };
-
-  if (widget.metadata.panel("Fields").items.length > 0) {
-    widget.metadata.panel("Fields").items.forEach(valueItem => {
-      dynamicParamsData.fieldsForDynamicParams[valueItem.jaql.dim] = {
-        title: valueItem.jaql.title,
-        value: 1
+prism.registerWidget(widgetName, {
+  name: widgetName,
+  family: widgetName,
+  title: widgetName,
+  iconSmall: "/plugins/" + widgetName + "/" + widgetName + "-icon-small.png",
+  styleEditorTemplate: "/plugins/" + widgetName + "/styler.html",
+  hideNoResults: true,
+  directive: {
+    desktop: widgetName
+  },
+  style: {
+    isRoundStrokes: true
+  },
+  data: {
+    selection: [],
+    defaultQueryResult: {},
+    panels: [{
+        name: 'Fields',
+        type: "visible",
+        metadata: {
+          types: ['measures'],
+          maxitems: -1
+        },
+        visibility: true
+      },
+      {
+        name: 'filters',
+        type: 'filters',
+        metadata: {
+          types: ['dimensions'],
+          maxitems: -1
+        }
+      }
+    ],
+    // builds a jaql query from the given widget
+    buildQuery: function (widget) {
+      // building jaql query object from widget metadata 
+      var query = {
+        datasource: widget.datasource,
+        format: "json",
+        isMaskedResult: true,
+        metadata: []
       };
 
-      dynamicParamsData.numOfValues++;
-    });
-  };
+      widget.numOfValues = 0;
+      widget.fieldsForDynamicParams = {};
 
-  registerWidgetsToBeforeQueryListener();
+      if (widget.metadata.panel("Fields").items.length > 0) {
+        widget.metadata.panel("Fields").items.forEach(valueItem => {
+          query.metadata.push(valueItem);
+          
+          widget.fieldsForDynamicParams[valueItem.jaql.dim] = {
+            title: valueItem.jaql.title,
+            value: 1
+          };
 
-  if (dynamicParamsData.numOfValues > 0) {
-    for (let key in dynamicParamsData.fieldsForDynamicParams) {
-      if (dynamicParamsData.fieldsForDynamicParams.hasOwnProperty(key)) {
-        let curParamVal = dynamicParamsData.fieldsForDynamicParams[key];
+          widget.numOfValues++;
+        });
+      };
 
-        let paramNameElm = document.createElement('h4');
-        paramNameElm.innerHTML = curParamVal.title;
-
-        let paramInputElm = document.createElement('input');
-        paramInputElm.id = key + 'DynamicParamInput';
-        paramInputElm.type = 'text';
-        paramInputElm.value = curParamVal.value;
-
-        dynamicParamsData.dynamicParamsInputs[key] = paramInputElm;
-
-        element.append(paramNameElm);
-        element.append(paramInputElm);
+      // pushing filters
+      if (defined(widget.metadata.panel("filters"), 'items.0')) {
+        widget.metadata.panel('filters').items.forEach(function (item) {
+          item = $$.object.clone(item, true);
+          item.panel = "scope";
+          query.metadata.push(item);
+        });
       }
+      return query;
+    },
+
+    processResult: function (widget, queryResult) {}
+  },
+  render: function (widget, event) {
+    // 	Get widget element, and clear it out
+    var element = $(event.element);
+    element.empty();
+
+    registerWidgetsToBeforeQueryListener();
+
+    prism.activeDashboard.dynamicParamsInputs = {};
+
+    if (widget.numOfValues > 0) {
+      for (let key in widget.fieldsForDynamicParams) {
+        if (widget.fieldsForDynamicParams.hasOwnProperty(key)) {
+          let curParamVal = widget.fieldsForDynamicParams[key];
+
+          let paramNameElm = document.createElement('h4');
+          paramNameElm.innerHTML = curParamVal.title;
+
+          let paramInputElm = document.createElement('input');
+          paramInputElm.id = key + 'DynamicParamInput';
+          paramInputElm.type = 'text';
+          paramInputElm.value = curParamVal.value;
+
+          prism.activeDashboard.dynamicParamsInputs[key] = paramInputElm;
+
+          element.append(paramNameElm);
+          element.append(paramInputElm);
+        }
+      }
+
+      let applyBtnElm = document.createElement('button');
+      applyBtnElm.addEventListener('click', applyDynamicParams);
+      applyBtnElm.className = 'btn';
+      applyBtnElm.style.padding = '2px';
+      applyBtnElm.innerText = "Apply";
+
+      let resetBtnElm = document.createElement('button');
+      resetBtnElm.addEventListener('click', resetDynamicParams);
+      resetBtnElm.className = 'btn';
+      resetBtnElm.style.padding = '2px';
+      resetBtnElm.innerText = "Reset";
+      resetBtnElm.style.marginLeft = '104px';
+
+      element[0].style.overflow = 'auto';
+
+      let brline = document.createElement('br');
+
+      element.append(brline);
+
+      element.append(applyBtnElm);
+      element.append(resetBtnElm);
     }
-
-    let applyBtnElm = document.createElement('button');
-    applyBtnElm.addEventListener('click', applyDynamicParams);
-    applyBtnElm.className = 'btn';
-    applyBtnElm.style.padding = '2px';
-    applyBtnElm.innerText = "Apply";
-
-    let resetBtnElm = document.createElement('button');
-    resetBtnElm.addEventListener('click', resetDynamicParams);
-    resetBtnElm.className = 'btn';
-    resetBtnElm.style.padding = '2px';
-    resetBtnElm.innerText = "Reset";
-    resetBtnElm.style.marginLeft = '104px';
-
-    element[0].style.overflow = 'auto';
-
-    let brline = document.createElement('br');
-
-    element.append(brline);
-
-    element.append(applyBtnElm);
-    element.append(resetBtnElm);
-  }
-}
+  },
+  options: {}
+});
 
 function registerWidgetsToBeforeQueryListener() {
   for (let index = 0; index < prism.activeDashboard.widgets.length; index++) {
@@ -84,7 +146,7 @@ function registerWidgetsToBeforeQueryListener() {
 }
 
 function handleWidgetBeforeQuerty(widget, query) {
-  if (dynamicParamsData.overrideValsConfig == null) {
+  if (prism.activeDashboard.overrideValsConfig == null) {
 
   } else {
     for (let index = 0; index < query.query.metadata.length; index++) {
@@ -94,10 +156,10 @@ function handleWidgetBeforeQuerty(widget, query) {
           if (curJaql.context.hasOwnProperty(key)) {
             let currContext = curJaql.context[key];
 
-            if (dynamicParamsData.overrideValsConfig.hasOwnProperty(currContext.dim)) {
+            if (prism.activeDashboard.overrideValsConfig.hasOwnProperty(currContext.dim)) {
               curJaql.formula = curJaql.formula.replace(
                 key,
-                dynamicParamsData.overrideValsConfig[currContext.dim]
+                prism.activeDashboard.overrideValsConfig[currContext.dim]
               );
             }
           }
@@ -108,13 +170,13 @@ function handleWidgetBeforeQuerty(widget, query) {
 }
 
 function applyDynamicParams(event) {
-  dynamicParamsData.overrideValsConfig = {};
+  prism.activeDashboard.overrideValsConfig = {};
 
-  for (let key in dynamicParamsData.dynamicParamsInputs) {
-    if (dynamicParamsData.dynamicParamsInputs.hasOwnProperty(key)) {
-      let curParamVal = dynamicParamsData.dynamicParamsInputs[key];
+  for (let key in prism.activeDashboard.dynamicParamsInputs) {
+    if (prism.activeDashboard.dynamicParamsInputs.hasOwnProperty(key)) {
+      let curParamVal = prism.activeDashboard.dynamicParamsInputs[key];
 
-      dynamicParamsData.overrideValsConfig[key] = curParamVal.value;
+      prism.activeDashboard.overrideValsConfig[key] = curParamVal.value;
     }
   }
 
@@ -128,7 +190,7 @@ function applyDynamicParams(event) {
 }
 
 function resetDynamicParams(event) {
-  dynamicParamsData.overrideValsConfig = null;
+  prism.activeDashboard.overrideValsConfig = null;
 
   for (let index = 0; index < prism.activeDashboard.widgets.length; index++) {
     let curWidToListen = prism.activeDashboard.widgets.$$widgets[index];
